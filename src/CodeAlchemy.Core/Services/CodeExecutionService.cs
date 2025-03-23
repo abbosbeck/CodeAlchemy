b612e428d61ee3cb3055721e9f460008c02e48cf
Abbosbeck <Abbosalisherovich2003@gmail.com>
2025-03-23 18:11:13 +0500

    Create Code Execution Service

---
+++ src/CodeAlchemy.Core/Services/CodeExecutionService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace CodeAlchemy.Core.Services
+{
+    public class CodeExecutionService
+    {
+        public async Task<string> ExecuteCodeAsync(string code)
+        {
+            try
+            {
+                var result = await CSharpScript.EvaluateAsync(code, ScriptOptions.Default);
+                return result?.ToString() ?? "No Output";
+            }
+            catch (Exception ex)
+            {
+                return $"Error: {ex.Message}";
+            }
+        }
+    }
+}
